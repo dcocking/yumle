733b4553cd99b4b4b1cf09a34fa632d1ee74d536
dcocking <dustin.cocking@gmail.com>
2022-10-15 19:25:25 -0400

    add base href to build command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
 import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
