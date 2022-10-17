952303b8ad54ffb9af2e33854d903082b9c65761
Lee Cocking <lcocking@gmail.com>
2022-10-16 22:00:30 -0400

    Converting game flow to pixelation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 import yumms from './yumms.json';
 
@@ -8,25 +8,26 @@ import yumms from './yumms.json';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  grid!: HTMLElement | null;
+  // Proper loading of canvas element from:
+  // https://medium.com/angular-in-depth/how-to-get-started-with-canvas-animations-in-angular-2f797257e5b4
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D;
 
-  INTERVAL_X = 10;
-  INTERVAL_Y = 10;
-  POSITIONS = this.INTERVAL_X * this.INTERVAL_Y;
+  PIXEL_SIZES = [50,40,30,20,10,1];
 
   yumms: any = {};
   imageObject: {} | null | unknown = null;
+  currentImage!: HTMLImageElement;
+  currentPixelSize: number = this.PIXEL_SIZES[0];
   guessHistory: Array<string> = [];
-  reveals = 0;
 
-  gridArray = [];
   currentIndex = 0;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.grid = document.getElementById('yummle')
+    this.ctx = this.canvas.nativeElement.getContext('2d', {willReadFrequently: true})!;
     this.getYumms();
   }
 
@@ -35,33 +36,60 @@ export class GameComponent implements OnInit {
     this.loadYummle();
   }
 
+  /**
+   * TODO
+   * 
+   * X Replace current grid image with pixelation
+   * X Tie failed guesses to next pixelation level
+   * - Limit guesses to only 5
+   * - Introduce mobile keyboard (see https://hodgef.com/simple-keyboard/demos/?d=mobile)
+   * - Look at blur transition between one pixel size and the next (see https://github.com/hughjdavey/ngx-image-blur)
+   * - Add history / state in cookie (will probably want to create a login at some point, maybe with https://supabase.com/)
+   */
+
+  /**
+   * Pixels the current image, at the current pixel size
+   */
+  pixelateImage() {
+    let w = this.currentImage.width;
+    let h = this.currentImage.height;
+    this.ctx.drawImage(this.currentImage,0,0);
+    let pixelArr = this.ctx.getImageData(0, 0, w, h).data;
+    // let sampleSize = 10;
+    
+    for (let y = 0; y < h; y += this.currentPixelSize) {
+      for (let x = 0; x < w; x += this.currentPixelSize) {
+        let p = (x + (y*w)) * 4;
+        
+        let fillStyile = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3];
+        this.ctx.fillStyle = fillStyile;    
+        this.ctx.fillRect(x, y, this.currentPixelSize, this.currentPixelSize);
+      }   
+    } 
+  }
+
+  /**
+   * Loads the yummle at current index, pixelates it, and displays it
+   */
   loadYummle() {
     this.getImage('assets/' + this.yumms[this.currentIndex].image).then((image) => {
-      this.imageObject = image;
-
-      // Build table and update html
-      var imageGridHTML = this.buildTable();
-      if(this.grid) {
-        this.grid.innerHTML = imageGridHTML;
-
-        // Apply the image to the table
-        this.applyImage();
-
-        // Reveal the first image
-        this.nextReveal();
-
-        this.updateScore();
-
-      }
+      this.currentImage = <HTMLImageElement>image;
+      this.pixelateImage();
+      this.updateScore();
     })
-
   }
 
-  resetScores() {
+  /**
+   * Resets the current game state
+   */
+  resetGame() {
     this.guessHistory = [];
-    this.reveals = 0;
+    this.currentPixelSize = this.PIXEL_SIZES[0];
   }
 
+  /**
+   * Updates current guess count and resets the field after a guess is submitted
+   */
   updateScore() {
     const totalGuesses = this.guessHistory.length;
     const guessTag = document.getElementById('guess-count');
@@ -70,110 +98,24 @@ export class GameComponent implements OnInit {
     guessInput!.value = '';
   }
 
-//logo - Blonde by Billy Argel from fontspace.com
+// Note: logo image - Blonde Font by Billy Argel from fontspace.com
 
   getImage(path: any) {
     const promise = new Promise((resolve) => {
-      var imageObject = {};
-      var image = new Image();
-      image.onload = () => {
-        imageObject = {
-          image: image,
-          xInterval: Math.round(image.width / this.INTERVAL_X),
-          yInterval: Math.round(image.height / this.INTERVAL_Y),
-        }
-        resolve(imageObject);
+      const img = new Image();
+      img.src = path;
+      img.onload = ()=>{
+        
+        resolve(img);
       }
-      image.src = path;
     })
-    return promise;
+    return promise;      
   }
 
-  buildTable() {
-    var table = '';
-    for (let y = 0; y < this.INTERVAL_Y; y++) {
-      table += '<div class="row">';
-      for (let x = 0; x < this.INTERVAL_X; x++) {
-        table += '<div class="cell">';
-        // table += `<img class="yummle-img" id='yummle-${y}-${x}' width="1" height="1" src="./white1x1.jpg"/>`;
-        table += `<div class="yummle-img" id='yummle-${y}-${x}'></div>`;
-        table += '</div>';
-      }
-      table += '</div>';
-    }
-
-    // console.log(table);
-
-    return table;
-  }
-
-  applyImage() {
-    for (let y = 0; y < this.INTERVAL_Y; y++) {
-      for (let x = 0; x < this.INTERVAL_X; x++) {
-        // console.log(`${y}, ${x} - ${imageObject.xInterval}, ${imageObject.yInterval}`)
-        const currentImage = document.getElementById(`yummle-${y}-${x}`);
-        // @ts-ignore
-        const positionX = -(x * this.imageObject.xInterval);
-        // @ts-ignore
-        const positionY = -(y * this.imageObject.yInterval);
-        // console.log(positionX,positionY)
-        // @ts-ignore
-        currentImage!.style.width = `${this.imageObject.xInterval}px`;
-        // @ts-ignore
-        currentImage!.style.height = `${this.imageObject.yInterval}px`;
-        // @ts-ignore
-        currentImage!.style.background = `url('${this.imageObject.image.src}') no-repeat 0 0`;
-        currentImage!.style.backgroundPosition = `${positionX}px ${positionY}px`;
-        currentImage!.style.visibility = 'hidden';
-        const index = this.gridArray.length;
-
-        // Push image piece into array
-        // @ts-ignore
-        this.gridArray.push({position: `${y}-${x}`, visible: false,})
-      }
-    }
-  }
-
-  nextReveal() {
-    this.reveals++;
-    const revealCount = document.getElementById('reveal-count');
-    revealCount!.innerText = String(this.reveals);
-    const index = this.generateRandomIndex();
-    this.revealIndex(index);
-  }
-
-  generateRandomIndex(maxLimit = this.POSITIONS) {
-    let nextIndex = -1;
-
-    let count = 0;
-    while (nextIndex === -1 && count <= this.POSITIONS + 1) {
-      let rand = Math.random() * maxLimit;
-      rand = Math.floor(rand); // 99
-
-      if (this.checkIndex(rand) === false) {
-        nextIndex = rand;
-      }
-      count++;
-    }
-
-    return nextIndex;
-  }
-
-  checkIndex(index: number) {
-    // @ts-ignore
-    return (this.gridArray)[index].visible;
-  }
-
-  revealIndex(index: number) {
-    // @ts-ignore
-    (this.gridArray)[index].visible = true;
-    // @ts-ignore
-    const position = (this.gridArray)[index].position;
-    let gridItem = document.getElementById(`yummle-${position}`);
-    // @ts-ignore
-    gridItem.style.visibility = 'visible';
-  }
-
+  /**
+   * Processes guesses typed into the guess input
+   * @param event Keyboard event
+   */
   guess(event?: KeyboardEvent) {
     var guess = '';
     if (event) {
@@ -188,20 +130,42 @@ export class GameComponent implements OnInit {
 
     if (guess !== '') {
       var validAnswer = false;
+      this.guessHistory.push(guess);      
 
       (this.yumms)[this.currentIndex].guesses.forEach((correctGuess: string) => {
         if (guess === correctGuess) {
           validAnswer = true;
         }
       })
-
-      // @ts-ignore
-      this.guessHistory.push(guess);
+      
       this.updateScore();
-      this.gameOver();
+      if (validAnswer) {
+        this.gameOver();
+        this.revealImage();
+        this.resetGame();
+      } else {
+        // @ts-ignore
+        this.nextPixelSize();
+        this.pixelateImage();
+      }
     }
   }
 
+  revealImage() {
+    this.currentPixelSize = this.PIXEL_SIZES[5];
+    this.pixelateImage();
+  }
+
+  /**
+   * Sets the current pixelation size to the correlated number of guesses
+   */
+  nextPixelSize() {
+    this.currentPixelSize = this.PIXEL_SIZES[this.guessHistory.length];
+  }
+
+  /**
+   * Handles game over state after correct guess
+   */
   gameOver() {
     const modalGuessScore = document.getElementById('modalGuessScore');
     const modalRevealScore = document.getElementById('modalRevealScore');
@@ -213,26 +177,19 @@ export class GameComponent implements OnInit {
       scoreText += 'guesses';
     }
     modalGuessScore!.innerText = scoreText;
-    var revealText = this.reveals + ' ';
-    if (this.reveals === 1) {
-      revealText += 'reveal';
-    } else {
-      revealText += 'reveals';
-    }
     const lastAnswer = (this.guessHistory)[this.guessHistory.length - 1];
     const modalAnswerText = lastAnswer[0].toUpperCase() + lastAnswer.substring(1);
-    modalRevealScore!.innerText = revealText;
     modalAnswer!.innerText = modalAnswerText;
     // @ts-ignore
     $('#exampleModalCenter').modal('show');
   }
 
+  /**
+   * Dev function to load the next yummle
+   */
   nextYummle() {
     this.currentIndex++;
-    while (this.gridArray.length > 0) {
-      this.gridArray.pop();
-    }
-    this.resetScores();
+    this.resetGame();
     this.loadYummle();
   }
 
