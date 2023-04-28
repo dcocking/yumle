a9bad9fb956e91b5f4a37130b4cada7972c39856
Lee Cocking <lcocking@gmail.com>
2023-04-28 16:30:32 -0400

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+// comment
 // Angular Material
 import { MatCardModule } from "@angular/material/card";
 import { MatDialogModule } from '@angular/material/dialog';
